1d6417dfc0134b69de195fdbac14498c5ad38bda
mrmeku <mrmeku@gmail.com>
2018-02-13 14:22:27 -0700

    Demo flex-layout incompatibility

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 
-import {NgModule, Component} from '@angular/core';
+import {Component, NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import {BrowserModule} from '@angular/platform-browser';
-import { HelloWorldModule } from './hello-world/hello-world.module';
 
-@Component({
-  selector: 'app-component',
-  template: '<hello-world-app></hello-world-app>'
-})
+import {HelloWorldModule} from './hello-world/hello-world.module';
+
+@Component({selector: 'app-component', template: '<hello-world-app></hello-world-app>'})
 export class BootstrapComponent {}
 
 @NgModule({
-  imports: [BrowserModule, HelloWorldModule],
+  imports: [
+    BrowserModule,
+    HelloWorldModule,
+    FlexLayoutModule,
+  ],
   declarations: [BootstrapComponent],
   bootstrap: [BootstrapComponent],
 })
