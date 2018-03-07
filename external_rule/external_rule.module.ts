a4346cd87d6acf295950da3bffe1a82aa11478c5
Daniel Muller <mrmeku@gmail.com>
2018-03-07 10:07:33 -0700

    Reproduce external_rule ng_module ngfactory path bug

---
+++ external_rule/external_rule.module.ts
@@ -0,0 +1,8 @@
+
+import {NgModule} from '@angular/core';
+import {SomeProvider} from './some_provider';
+
+@NgModule({
+  providers: [SomeProvider],
+})
+export class ExternalRuleModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {ExternalRuleModule} from '@external_rule/module';
 
 import {AppComponent} from './app.component';
 import {HelloWorldModule} from './hello-world/hello-world.module';
 
 @NgModule({
-  imports: [BrowserModule, HelloWorldModule],
+  imports: [BrowserModule, HelloWorldModule, ExternalRuleModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
